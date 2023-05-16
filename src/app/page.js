c04d3d1afe6e1d5d0314d28ceb688e55c3d6e2a0
lightmyfire17 <andrtaw1@gmail.com>
2023-05-16 17:58:33 +0300

    feat: add use cases

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,30 @@ export default function Home() {
     setMenu(!menuOpen);
   };
 
+  const cases = [
+    {
+      icon: "collectors",
+      title: "For Collectors",
+      subtitle: "Buy NFTs Now,Pay for Them Later",
+      descr:
+        "Access and use highly sought-after NFTs without paying the full price upfront",
+    },
+    {
+      icon: "artists",
+      title: "For ARTISTS",
+      subtitle: "Increase access to your art without sacrificing its value",
+      descr:
+        "It’s common-place to finance art purchases at Sotheby's and Christies",
+    },
+    {
+      icon: "marketplaces",
+      title: "For Marketplaces",
+      subtitle: "Boost Sales with Marketplace Fees Paid Up Front",
+      descr:
+        "Give your users options, increase sales, and get your marketplace fee paid at the time of sale.",
+    },
+  ];
+
   return (
     <div className="content">
       <header className="header">
@@ -77,6 +101,13 @@ export default function Home() {
         </nav>
       </div>
       <main className="main">
+        <img className="box box-1" src="/box-1.png" alt="decor" />
+        <img className="box box-2" src="/box-2.png" alt="decor" />
+        <img className="box box-3" src="/box-3.png" alt="decor" />
+        <img className="box box-4" src="/box-4.png" alt="decor" />
+        <img className="box box-5" src="/box-5.png" alt="decor" />
+        <img className="box box-6" src="/box-6.png" alt="decor" />
+        <img className="box box-7" src="/box-7.png" alt="decor" />
         <section className="section intro">
           <h1 className="intro__title">
             <span>Buy now,</span>
@@ -128,6 +159,33 @@ export default function Home() {
           <h2 className="section__title">products</h2>
           <img className="products__img" src="/products.png" alt="product" />
           <div className="products__gradient"></div>
+        </section>
+
+        <section className="section cases">
+          <div className="cases__top">
+            <h3 className="section__subtitle">Use Cases</h3>
+            <p className="cases__descr section__descr">
+              Financing works for every asset, every user, in every use-case.
+            </p>
+          </div>
+
+          <div className="cases__grid">
+            {cases.map((x, i) => (
+              <div key={i} className="cases__card">
+                <Image
+                  className="cases__card-icon"
+                  src={"./icons/" + x.icon + ".svg"}
+                  alt="collectors"
+                  width={24}
+                  height={24}
+                />
+                <h5 className="cases__card-title">{x.title}</h5>
+                <div className="cases__card-subtitle"> {x.subtitle}</div>
+                <div className="cases__card-divider"></div>
+                <div className="cases__card-descr">{x.descr}</div>
+              </div>
+            ))}
+          </div>
         </section>
       </main>
     </div>
